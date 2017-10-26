03f596e4b474e841c0779823a59baac6f6fdd744
Roman Sveshnikov <romansvesh@gmail.com>
2017-10-27 00:12:12 +0400

    refactoring fix locators fix name of POM fix name of helper move wait in sign_in_pagy

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 
 import unittest
-from pom import base_page, main_page, sign_in_page
+
 from selenium import webdriver
+from helper import helper
+from page import main_page, sign_in_page
 
 
 class TestLogin(unittest.TestCase):
@@ -12,21 +14,22 @@ class TestLogin(unittest.TestCase):
         self.driver.maximize_window()
 
     def test_login(self):
-        base = base_page.BasePage(self.driver)
+        help = helper.Helper(self.driver)
         main = main_page.MainPage(self.driver)
         sign_in = sign_in_page.SignInPage(self.driver)
 
-        if base.is_cookie_file_exist():
+        if help.is_cookie_file_exist():
             main.login_with_cookie()
         else:
             main.press_button_sign_in()
             sign_in.login()
-            base.save_cookie()
+            help.save_cookie()
 
         self.assertTrue(main.gravatar_presence())
 
     def tearDown(self):
         self.driver.close()
+
 
 
 if __name__ == "__main__":
