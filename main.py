74153e19406231457f9b048c2dd51d137cca1f50
Roman Sveshnikov <romansvesh@gmail.com>
2017-10-29 21:04:04 +0400

    refactoring

--- main.py
+++ main.py
@@ -3,8 +3,9 @@
 import unittest
 
 from selenium import webdriver
-from helper import helper
-from page import main_page, sign_in_page
+
+from helper.helper import Helper
+from page.main_page import MainPage
 
 
 class TestLogin(unittest.TestCase):
@@ -14,22 +15,15 @@ class TestLogin(unittest.TestCase):
         self.driver.maximize_window()
 
     def test_login(self):
-        help = helper.Helper(self.driver)
-        main = main_page.MainPage(self.driver)
-        sign_in = sign_in_page.SignInPage(self.driver)
+        help_ = Helper(self.driver)
+        main = MainPage(self.driver)
 
-        if help.is_cookie_file_exist():
-            main.login_with_cookie()
-        else:
-            main.press_button_sign_in()
-            sign_in.login()
-            help.save_cookie()
+        help_.login()
 
         self.assertTrue(main.gravatar_presence())
 
     def tearDown(self):
         self.driver.close()
-
 
 
 if __name__ == "__main__":
--- page/sign_in_page.py
+++ page/sign_in_page.py
@@ -1,21 +1,24 @@
 # -*- coding: utf-8 -*-
 
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.wait import WebDriverWait
+
 from constants.constants import LOGIN, PASSWORD
 from helper.helper import Helper
-from selenium.webdriver.support import expected_conditions
 
 
-class SignInPage(Helper):
+class SignInPage:
+
+    def __init__(self, driver):
+        self._driver = driver
+
     # Locators
     INPUT_EMAIL = (By.ID, 'user_email')
     INPUT_PASSWORD = (By.ID, 'user_password')
     BUTTON_SUBMIT = (By.XPATH, '//*[@type="submit"]')
 
     def fill_email(self):
-        wait = WebDriverWait(self._driver, 5)
-        wait.until(expected_conditions.element_to_be_clickable((self.INPUT_EMAIL[0], self.INPUT_EMAIL[1])))
+        help_ = Helper(self._driver)
+        help_.wait_for_element(5, self.INPUT_EMAIL)
         element = self._driver.find_element(*self.INPUT_EMAIL)
         element.send_keys(LOGIN)
 
@@ -27,7 +30,4 @@ class SignInPage(Helper):
         element = self._driver.find_element(*self.BUTTON_SUBMIT)
         element.click()
 
-    def login(self):
-        self.fill_email()
-        self.fill_password()
-        self.press_button_submit()
+
